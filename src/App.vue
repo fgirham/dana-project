6d013853e9affbe307af2879569552dfda0d9612
fgirham <>
2022-04-06 09:47:58 +0700

    feature: create footer component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Navbar from '@/components/Navbar.vue'
+import FooterComp from '@/components/FooterComp.vue'
 </script>
 
 <template>
   <Navbar />
   <RouterView />
+  <FooterComp />
 </template>
 
 <style>
---
+++ src/components/FooterComp.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+<footer>
+    <div class="flex-container">
+        <div class="flex-item-left">
+            <p>Follow Us:</p>
+            <div class="icons">
+                <a target="_blank" href="https://www.facebook.com/danawallet/"><i class="fa fa-facebook-square fa-lg"></i></a>
+                <a target="_blank" href="https://www.instagram.com/dana.id/"><i class="fa fa-instagram fa-lg"></i></a>
+                <a target="_blank" href="https://twitter.com/danawallet/"><i class="fa fa-twitter fa-lg"></i></a>
+            </div>
+        </div>
+        <div class="flex-item-right">
+            <div class="footer-link">
+                <RouterLink to="/about">ABOUT</RouterLink>
+                <RouterLink to="/contact">CONTACT US</RouterLink>
+            </div>
+            <p>&copy; 2017 DANA - PT Espay Debit Indonesia Koe. All Rights Reserved</p>
+        </div>
+    </div>
+</footer>
+</template>
+
+<style scoped lang="less">
+@import '@/assets/footer.less';
+</style>
