92ba7d08edc92fdf4e39249d607ec4db4ca7a1f5
fgirham <>
2022-04-07 11:54:05 +0700

    feature: create form component

---
+++ src/components/InputComp.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    isTextArea: Boolean,
+    label: String    
+})
+</script>
+
+<template>
+<label>{{ label }}</label>
+<input v-if="!isTextArea" v-bind="$attrs"/>
+<textarea v-if="isTextArea" v-bind="$attrs"></textarea>
+</template>
+
+<style scoped lang="less">
+    label {
+        display: block;
+        margin-bottom: 5px;
+    }
+    input {
+        width: 100%;
+        padding: 10px 10px;
+        margin-bottom: 20px;
+    }
+    textarea {
+        width: 100%;
+        font-family: inherit;
+        min-height: 200px;
+        padding: 10px 10px;
+        margin-bottom: 20px;
+    }
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,20 @@
 <script setup>
+import InputComp from '../components/InputComp.vue'
 </script>
 
 <template>
+    <div class="flex-container">
+        <div class="flex-item1">
+            <h1 class="section-title">Contact Us</h1>
+            <form>
+                <InputComp label="Name" type="text" />
+                <InputComp label="E-mail" type="text" />
+                <InputComp :isTextArea="true" label="Message" />
+            </form>
+        </div>
+    </div>
 </template>
+
+<style scoped lang="less">
+@import '@/assets/contact.less';
+</style>
