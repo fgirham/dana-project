e6174b6100c1c65ad2a4aa2665076711fac30694
fgirham <54946210+fgirham@users.noreply.github.com>
2022-04-18 13:21:50 +0700

    Feat: create contact page (#5)
    * feature: create form component
    
    * feature: add contact and social media
    
    * feature: create button component
    
    * feature: configure event for components
    
    * feature: contact responsive view
    
    Co-authored-by: fgirham <>

---
+++ src/components/ButtonComp.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { defineProps, computed } from 'vue';
+
+const props = defineProps({
+    color: String,
+    buttonText: String,
+})
+
+const buttonColor = computed(() => {
+    return {
+        '--bg-color': props.color,
+    }
+})
+</script>
+
+<template>
+    <button :style="buttonColor" @click="$emit('onClick')">{{ buttonText }}</button>
+</template>
+
+<style scoped lang="less">
+button {
+    background-color: var(--bg-color);
+    border: none;
+    color: white;
+    padding: 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    width: 100%;
+    font-family: inherit;
+}
+</style>
---
+++ src/components/InputComp.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    isTextArea: Boolean,
+    label: String,
+    modelValue: [String, Number],
+})
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <label>{{ label }}</label>
+    <input
+        v-if="!isTextArea"
+        v-bind="$attrs"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+    />
+    <textarea
+        v-if="isTextArea"
+        v-bind="$attrs"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+    ></textarea>
+</template>
+
+<style scoped lang="less">
+label {
+    display: block;
+    margin-bottom: 5px;
+}
+input {
+    width: 100%;
+    padding: 10px 10px;
+    margin-bottom: 20px;
+}
+textarea {
+    width: 100%;
+    font-family: inherit;
+    min-height: 200px;
+    padding: 10px 10px;
+    margin-bottom: 20px;
+}
+</style>
