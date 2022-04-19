e51f281ac2227a80f6a7d07ca8a3dd0bf5789df7
fgirham <>
2022-04-19 09:40:10 +0700

    feat: add photo grid component

---
+++ src/components/PhotoGrid.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="photo-container">
+    <img class="item" v-for="photo in photos" :key="photo.id" :src="photo.thumbnailUrl">
+</div>
+</template>
+
+<script>
+export default {
+    name: 'PhotoGrid',
+
+    data() {
+        return {
+            photos: [],
+        }
+    },
+
+    methods: {
+        fetchPhotos() {
+            this.axios.get('https://jsonplaceholder.typicode.com/photos')
+                .then((response) => { this.photos = response.data })
+                .then(console.log(this.photos))
+        }
+    },
+
+    created() {
+        this.fetchPhotos()
+    }
+}
+</script>
+
+<style scoped lang="less">
+.photo-container {
+  display: flex;
+  padding: 10px;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  .item {
+      margin: 20px;
+  }
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Hero from '@/components/Hero.vue';
+import PhotoGrid from '@/components/PhotoGrid.vue';
 </script>
 
 <template>
   <main>
     <Hero />
+    <PhotoGrid />
   </main>
 </template>
